fc0aa03e12e72af7cdad1915f39f3e2191e07dc7
bdel09 <bmcdelbarrio@gmail.com>
2025-09-24 22:18:35 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,12 +1,14 @@
 #Name: Benjamin Del Barrio
 #Email: benjamin.delbarrio31@myhunter.cuny.edu
 import turtle
-t = turtle.Turtle()
+
 def stamp_colors():
-    for i in range(5);
-        color = input("Enter color (as hex): ")
-        t.color(color)     
-        t.forward(20)
-        t.stamp()
+t = turtle.Turtle()
+t.shape("turtle)
+for i in range(5);
+    color = input('Enter color (as hex): ')
+    t.color(color)     
+    t.forward(20)
+    t.stamp()
 stamp_colors()
   
