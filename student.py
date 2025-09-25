51ff2e04a473139021046b2dc9595ea2eca0d408
bdel09 <bmcdelbarrio@gmail.com>
2025-09-24 22:14:02 -0400

    Create student.py

---
+++ student.py
@@ -0,0 +1,12 @@
+#Name: Benjamin Del Barrio
+#Email: benjamin.delbarrio31@myhunter.cuny.edu
+import turtle
+t = turtle.Turtle()
+def stamp_colors():
+    for i in range(5);
+        color = input("Enter color (as hex): ")
+        t.color(color)     
+        t.forward(20)
+        t.stamp()
+stamp_colors()
+  
